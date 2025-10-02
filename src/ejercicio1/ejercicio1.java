231c3e4e2a832b9a0838d39ed7bda4ba891232bd
Amanda <amandajaneirovazquez95@liceolapaz.net>
2025-10-02 09:38:17 +0200

    proyecto creado

---
+++ src/ejercicio1/ejercicio1.java
@@ -0,0 +1,13 @@
+package ejercicio1;
+
+import java.util.Scanner;
+
+public class ejercicio1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Ingrese su edad");
+        int edad=sc.nextInt();
+        //
+        
+    }
+}
