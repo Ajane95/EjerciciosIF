e87e4b85177d14f639b6ca6ed3fa13e4bf976caa
Amanda <amandajaneirovazquez95@liceolapaz.net>
2025-10-02 10:13:57 +0200

    ejercicio 1 acabado,pendiente corregir

--- src/ejercicio1/ejercicio1.java
+++ src/ejercicio1/ejercicio1.java
@@ -8,6 +8,17 @@ public class ejercicio1 {
         System.out.println("Ingrese su edad");
         int edad=sc.nextInt();
         //
-        
+        if(edad < 5){
+            System.out.println("La entrada es gratis");
+        } else{
+            if(edad>=5 && edad<=12) {
+                System.out.println("La entrada tiene 50% de descuento");
+            } else if(edad>=13 && edad<=64) {
+                System.out.println("La entrada no tiene descuento");
+            } else if (edad >= 65) {
+                System.out.println("La entrada tiene un 75% de descuento");
+            }
+
+        }
     }
 }
